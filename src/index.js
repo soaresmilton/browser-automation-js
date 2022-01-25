e2feb10e419875c36122d4bc0797a2363d1c720d
soaresmilton <eng.milton.soares@gmail.com>
2022-01-25 15:19:04 -0300

    Add scrapping function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('Hello Script')
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://quotes.toscrape.com/');
+
+  const grabQuotes = await page.evaluate(() => {
+    const quotes = document.querySelectorAll('.quote');
+
+    let quotesArray = [];
+
+    quotes.forEach(quoteTag => {
+      const quoteInfo = quoteTag.querySelectorAll('span');
+      const actualQuote = quoteInfo[0];
+      const actualAuthor = quoteInfo[1];
+
+      const authorName = actualAuthor.querySelector('small');
+
+      quotesArray.push({
+        quote: actualQuote.innerText,
+        author: authorName.innerText
+      });
+
+    });
+
+    return quotesArray;
+
+  });
+
+  console.log(grabQuotes);
+
+  await browser.close();
+
+})();
