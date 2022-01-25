7f9441958fd0925b613d23c3ee15bd58e32b284d
soaresmilton <eng.milton.soares@gmail.com>
2022-01-25 15:29:17 -0300

    Add automate login function

---
+++ src/login.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+async function login() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://quotes.toscrape.com/');
+
+
+  await page.click('a[href="/login"]');
+  await page.focus('#username');
+  await page.keyboard.type('user');
+  await page.focus('#password');
+  await page.keyboard.type('dskas98d023nco823');
+  await page.click('input[value="Login"]');
+
+  await page.evaluate(() => {
+    window.alert('Você está logado!');
+  });
+
+
+
+  // await browser.close();
+};
+
+login();
